33315ab205de822e4682a8f4d12235fcee43b649
Shuyuan <779455514@qq.com>
2021-02-23 12:23:41 +0100

    change addr api for test

--- client/src/app/services/message.service.ts
+++ client/src/app/services/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '../app.message';
 
-const baseUrl = 'http://localhost:3080/api/messages';
+const baseUrl = 'http://139.162.199.44:3080/api/messages';
 
 @Injectable({
   providedIn: 'root'
