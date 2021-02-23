359c069d8636fab6e7aa117b828109a4cc2831e3
Shuyuan <779455514@qq.com>
2021-02-23 16:52:17 +0100

    add functuion back-end : add new message into db

--- server/router.js
+++ server/router.js
@@ -5,9 +5,6 @@ var Comment = require('./Model/Comment')
 var timeDiff = require('./js/timeDiff')
 var count = 0
 
-router.get('/', (req, res) => {
-    res.send('App Works !!!!');
-});
 router.get('/api/messages', async(req, res) => {
     console.log("api/messages" + (count++));
 
@@ -22,5 +19,43 @@ router.get('/api/messages', async(req, res) => {
         res.status(400).send(err)
     }
 })
+router.post('/api/newMessage', function(req, res) {
+        new Comment(req.body).save()
+            .then(function(data) {
+                res.send('ok')
+            }, function(err) {
+                console.log(err)
+                res.status(500).send(err)
+            })
+    })
+    /* router.get('/api/messages/edit', function(req, res) {
+        Comment.findById(req.query._id).lean().exec(function(err, comment) {
+            if (err)
+                return res.status(500).send('Server error')
+            res.send(comment)
+        })
+    })
+    router.post('/api/messages/edit', function(req, res) {
+
+        Comment.findOneAndUpdate({ _id: req.body._id }, {
+                title: req.body.title,
+                message: req.body.message,
+                editTime: new Date
+            },
+            function(err, ret) {
+                if (err) {
+                    return res.status(500).send('Server error')
+                }
+                res.send('ok')
+            })
+    })
+    router.get('/api/messages/delete', function(req, res) {
+        Comment.findByIdAndDelete(req.query._id)
+            .then(function(ret) {
+                res.send('ok')
+            }, function(err) {
+                return res.status(500).send('Server error')
+            })
+    }) */
 
 module.exports = router
